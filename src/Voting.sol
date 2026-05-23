6807a204ffdfc81a1210fee53aaf9b77a84c0ea7
alcaniz <alcaniz.rst@gmail.com>
2026-05-23 20:52:36 +0200

    feat: add Voting contract skeleton from specification

--- src/Voting.sol
+++ src/Voting.sol
@@ -1,14 +1,38 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.35;
 
-contract Counter {
-    uint256 public number;
+import {Ownable} from "..lib/openzeppelin-contracts/access/Ownable.sol"
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+contract Voting is Ownable {
 
-    function increment() public {
-        number++;
-    }
+enum WorkflowStatus {
+    RegisteringVoters,
+    ProposalsRegistrationStarted,
+    ProposalsRegistrationEnded,
+    VotingSessionStarted,
+    VotingSessionEnded,
+    VotesTallied
 }
+
+event VoterRegistered(address voterAddress);
+event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+event ProposalRegistered(uint proposalId);
+event Voted(address voter, uint proposalId);
+
+struct Voter {
+    bool isRegistered;
+    bool hasVoted;
+    uint votedProposalId;
+}
+
+struct Proposal {
+    string description;
+    uint voteCount;
+}
+
+uint winningProposalId;
+
+}
+
+
+
