a910a0e51488c12f8fda5b2f16a2fa0313478de6
alcaniz <alcaniz.rst@gmail.com>
2026-05-24 10:27:26 +0200

    style: format Solidity files with forge fmt

--- src/Voting.sol
+++ src/Voting.sol
@@ -4,38 +4,33 @@ pragma solidity ^0.8.35;
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract Voting is Ownable {
-
     constructor() Ownable(msg.sender) {}
 
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
 
-enum WorkflowStatus {
-    RegisteringVoters,
-    ProposalsRegistrationStarted,
-    ProposalsRegistrationEnded,
-    VotingSessionStarted,
-    VotingSessionEnded,
-    VotesTallied
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event ProposalRegistered(uint256 proposalId);
+    event Voted(address voter, uint256 proposalId);
+
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint256 votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint256 voteCount;
+    }
+
+    uint256 winningProposalId;
 }
-
-event VoterRegistered(address voterAddress);
-event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
-event ProposalRegistered(uint proposalId);
-event Voted(address voter, uint proposalId);
-
-struct Voter {
-    bool isRegistered;
-    bool hasVoted;
-    uint votedProposalId;
-}
-
-struct Proposal {
-    string description;
-    uint voteCount;
-}
-
-uint winningProposalId;
-
-}
-
-
 
