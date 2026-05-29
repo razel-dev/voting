1a77ed71f15fd4b7a7827fb8b4903220e1fcf097
alcaniz <alcaniz.rst@gmail.com>
2026-05-29 13:14:58 +0200

    feat: add onlyVoter modifier

--- src/VotingPlus.sol
+++ src/VotingPlus.sol
@@ -3,4 +3,10 @@ pragma solidity ^0.8.35;
 
 import "./Voting.sol";
 
-contract VotingPlus is Voting {}
+contract VotingPlus is Voting {
+    modifier onlyVoter() {
+        require(voters[msg.sender].isRegistered, "Voter must be registered");
+
+        _;
+    }
+}
